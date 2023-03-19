9c91f46dac0e6a4520adc60ee699ef4fe42dde11
Kristiana12 <kristiana.1992@hotmail.com>
2023-03-19 08:51:51 +0100

    user styling

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -1,7 +1,15 @@
-import "./user.css";
+import './user.css';
 
 function User() {
-  return <div className="user">User</div>;
+  return (
+    <header className="user-info">
+      <div className="user-info__profile-pic"></div>
+      <div className="user-info__content">
+        <p className="user-info__content--greeting">Welcome back,</p>
+        <h1 className="user-info__content--username">John Doe</h1>
+      </div>
+    </header>
+  );
 }
 
 export default User;
